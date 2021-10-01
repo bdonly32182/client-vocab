78e6ade2904ab98ee504ec531185b46c90d360a3
Tivakorn <not_not_not1@hotmail.co.th>
2021-10-02 02:53:05 +0700

    Edit login page use hook from.

--- src/page/Login.js
+++ src/page/Login.js
@@ -1,62 +1,65 @@
-import React,{Component} from 'react'
-import {connect} from 'react-redux'
+// Libs
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+
+// Components
 import LoginForm from '../Form/LoginForm'
-import {login} from '../action'
-import ListWord from './ListWord'
-import Welcome from './Welcome'
-import Header from '../component/Header'
 
-class Login extends Component{
-   async componentDidMount(){
-        
-    let Token =await localStorage.getItem('token')
-     if (Token) {
-        this.props.history.push('/listAnswer')
+const Login = props => {
 
-     }
-    }
-   
-    render(){
-        const {formValue,login,users,history} = this.props
-        console.log(users);
-        return(
-            <div className="container-fluid p-3 mb-2 bg-warning">
-             
-                {!users.token &&
+    const [token, setToken] = useState('')
+
+    const formValue = useSelector(state => state.formValue)
+    const login = useSelector(state => state.login)
+    const users = useSelector(state => state.users)
+
+    const history = useHistory()
+
+    useEffect(() => {
+        (async() => {
+
+            const token = await localStorage.getItem('token')
+
+            if (token) history.push('/listAnswer')
+        })()
+    }, [])
+
+    return (
+        <div className="container-fluid p-3 mb-2 bg-warning">
+            {!users.token &&
                 <div className="row">
                     <div className="col">
-                                
+
                     </div>
                     <div className="col border border-secondary shadow-lg p-3 mb-5 bg-info rounded">
-                       {users.user_normal||users.msg? <div><h1 className="text-danger">Please Check email or password</h1></div>:null}
-        
-                                 <h1 className="text-center text-white">
-                                         Login 
-                                </h1>
-                                <LoginForm  onSubmitLogin={()=>login(formValue,history)} />
-                       
+                        {users.user_normal || users.msg ? <div><h1 className="text-danger">Please Check email or password</h1></div> : null}
+
+                        <h1 className="text-center text-white">
+                            Login
+                        </h1>
+                        <LoginForm onSubmitLogin={() => login(formValue, history)} />
+
                     </div>
                     <div className="col">
-                                 
+
                     </div>
                 </div>
-              
+
             }
-             <br />
-             <br />                  
-             <br />
-             <br />
-             <br />
-             <br />
-             <br />
-             <br />
-             <br />
-             <br />
-            </div>
-        )
-    }
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+        </div>
+    )
+    
 }
-function mapStateToProps({form,users}){
-    return {formValue:form.LoginForm?form.LoginForm.values:null,users:users}
-}
-export default connect(mapStateToProps,{login})(Login)
+
+export default Login
